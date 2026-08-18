c0fb1f4eecb787b7418a50fd83eb2ce3bdd68f78
Anvith kanchan <anvithkanchan1@gmail.com>
2026-08-18 17:30:35 +0530

    Publish fortisec project

---
+++ src/fortisec/constants.py
@@ -0,0 +1,16 @@
+"""Shared constants for fortisec."""
+
+SECURITY_HEADERS: tuple[str, ...] = (
+    "Strict-Transport-Security",
+    "Content-Security-Policy",
+    "X-Frame-Options",
+    "X-Content-Type-Options",
+    "Referrer-Policy",
+    "Permissions-Policy",
+    "Cross-Origin-Embedder-Policy",
+    "Cross-Origin-Opener-Policy",
+    "Cross-Origin-Resource-Policy",
+)
+
+DEFAULT_REDACTION_PLACEHOLDER = "********"
+
