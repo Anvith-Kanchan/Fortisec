e3d30ebcbac79fbc5e1e4404651d3502c13cc3e0
Anvith kanchan <anvithkanchan1@gmail.com>
2026-08-18 17:30:38 +0530

    Publish fortisec project

---
+++ src/fortisec/exceptions.py
@@ -0,0 +1,18 @@
+"""Custom exceptions for fortisec."""
+
+
+class FortisecError(Exception):
+    """Base exception for fortisec errors."""
+
+
+class HeaderScanError(FortisecError):
+    """Raised when HTTP headers cannot be scanned."""
+
+
+class ValidationError(FortisecError):
+    """Raised for validation related errors."""
+
+
+class PathTraversalError(FortisecError):
+    """Raised when a path traversal attempt is detected."""
+
